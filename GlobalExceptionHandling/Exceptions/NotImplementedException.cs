52917eff50bf216dfd97d11628717aa48a7a5b68
mdabusayedislam <abusayediit@gmail.com>
2022-08-09 13:43:16 +0600

    Add project files.

---
+++ GlobalExceptionHandling/Exceptions/NotImplementedException.cs
@@ -0,0 +1,8 @@
+﻿namespace GlobalExceptionHandling.Exceptions
+{
+    public class NotImplementedException : Exception
+    {
+        public NotImplementedException(string message) : base(message)
+        { }
+    }
+}
